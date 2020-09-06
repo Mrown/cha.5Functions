60bbc8f6cc66d6b8cc597d3357014eeb26095d94
Mrown <70544258+Mrown@users.noreply.github.com>
2020-09-06 20:46:58 +0200

    Add files via upload

---
+++ jsfuncs0.js
@@ -0,0 +1,24 @@
+var b = 1.25;
+var a = prompt();
+function addVat(a) {
+  return a * b;
+}
+document.write(addVat(a));
+/*
+var b = 0.8;
+var a = prompt();
+function subVat(a) {
+  a * b;
+  return a * b;
+}
+document.write(subVat(a));
+*/
+/*
+var b = 0.25;
+var a = prompt();
+
+function calcVat(a) {
+  return a * b;
+}*/
+
+document.write(calcVat(a));
---
+++ jstextutils.js
@@ -0,0 +1,28 @@
+var s = prompt("");
+function first(s) {
+  return s[0];
+}
+document.write(first(s));
+
+/*
+var s = prompt("");
+function last(s) {
+	return s[s.length-1];
+}
+document.write("This is the last letter: " + last(s));
+*/
+/*
+var s = prompt("");
+function middle(s) {
+  return [1 - s.lenght - 1];
+}
+document.write(middle(s));
+*/
+/*
+var s = prompt("");
+
+function middle(s) {
+  return s.slice(1, -1);
+}
+document.write(middle(s));
+*/
